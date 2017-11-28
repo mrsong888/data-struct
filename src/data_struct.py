e38d993614a41e751399fbc607c0765420675e52
mrsong <hengheng.song@capitalcloud.net>
2017-11-28 18:51:28 +0800

    update

--- src/data_struct.py
+++ src/data_struct.py
@@ -1,6 +1,41 @@
 # !/usr/bin/env python
 # *-* coding:utf-8 *-*
 
+import heapq
+import threading
 
-class BaseSearch(object):
+class ExistError(Exception):
     pass
+
+class Tools(object):
+    pass
+
+
+class _PriorityQueue(object):
+
+    def __init__(self):
+        self._queue = []
+        self._sentinel = 0
+        self._task_lock = threading.RLock()
+        self.finder_dict = {}
+
+    def add_tasks(self, sequeue):
+        pass
+
+    def add_task(self, task, priority=0, reverse=False):
+        with self._task_lock:
+            if task in self.finder_dict:
+                raise ExistError('the task exist')
+            entry = [priority, self._sentinel, task]
+            self._sentinel += 1
+            self.finder_dict[task] = entry
+            heapq.heappush(self._queue, entry)
+
+    def pop_task(self):
+        with self._task_lock:
+            pass
+
+    def modify_priority(self, task, priority=0):
+        with self._task_lock:
+            if task in self.finder_dict:
+                entry = self.finder_dict[task]
